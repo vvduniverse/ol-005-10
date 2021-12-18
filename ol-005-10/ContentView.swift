e5e577a37b8041fde062c0c9fec11a80c19943a4
Vahtee Boo <ronnie.kaa@bigmir.net>
2021-12-18 19:59:18 +0200

    Initial commit +

--- ol-005-10/ContentView.swift
+++ ol-005-10/ContentView.swift
@@ -8,9 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var name = ""
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Form {
+            TextField("Enter your name", text: $name)
+            Text("Your name is \(name)")
+        }
     }
 }
 
